a7366f35ce4ea0f6720d7b91b7f1e526ad3d6373
Yuexin <yuexinli1203@gmail.com>
2024-07-29 23:39:35 -0700

    home page banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React from "react";
+
+import "../styles/home.css";
 
 export default function Home() {
   return (
     <div>
       Home
-      <div>
-        <h1>See every thing with Clarity</h1>
-        <p>Buying eyewear should leave you happy and good-looking, with money in your pocket. Glasses, sunglasses, and contacts—we've got your eyes covered.</p>
+      <div className="banner">
+        <div className="content">
+          <h1><strong>See</strong> everything <br /> with <strong>Clarity</strong></h1>
+          <p>
+            Buying eyewear should leave you happy and good-looking, with money
+            in your pocket. Glasses, sunglasses, and contacts—we've got your
+            eyes covered.
+          </p>
+          <br />
+          <a href="" className="button">
+            Shop Now
+            <span class="material-symbols-outlined">arrow_forward</span>
+          </a>
+        </div>
+        <div className="banner-img-wrapper">
+          <div className="banner-img"></div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
