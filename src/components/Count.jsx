e8781b98651b08fd0f23a30b2154a022d6126859
Yuexin <yuexinli1203@gmail.com>
2024-08-18 23:51:17 -0700

    add and subtract count

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { add, subtract} from '../store/counter';
 
 export default function Count() {
-
+  // useDispatch to change the state
+  const dispatch = useDispatch()
   const count = useSelector((state) => (state.counter.count))
+
+  function addCountFn() {
+    dispatch(add(1))
+  }
+
+  function subtractCountFn() {
+    dispatch(subtract(1))
+  }
 
   return (
     <div>
       <h2>Count: {count}</h2>
-      <button >addCount</button>
-      <button>subtractCount</button>
+      <button onClick={addCountFn}>addCount</button>
+      <button onClick={subtractCountFn}>subtractCount</button>
     </div>
   );
 }
--- src/store/counter.js
+++ src/store/counter.js
@@ -6,13 +6,19 @@ const counterSlice = createSlice({
         count: 1
     },
     reducers: {
+        // action:{type,payload}
         add(state, action){
-            console.log(state, action);
+            state.count += action.payload
+        },
+        subtract(state, action) {   
+            if (state.count > 0) {
+                state.count -= action.payload;
+            }
         }
     }
 })
 
-export const {add} = counterSlice.actions
+export const { add,subtract } = counterSlice.actions
 
 const counterReducer = counterSlice.reducer
 export default counterReducer
